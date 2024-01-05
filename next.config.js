dc59e7daf982a2b94a0e28e45cc02b84d66fea10
Oleksander Novak <sandrix28101981@gmail.com>
2024-01-05 11:15:47 +0200

    fixed next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
+    basePath: "/first-Next.js-project",
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/i,
