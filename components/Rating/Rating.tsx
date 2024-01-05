e8c7daf3ca758eea7d8f67ef85316cc4b382e2c2
Oleksander Novak <sandrix28101981@gmail.com>
2024-01-05 10:49:24 +0200

    github deploy

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -18,6 +18,7 @@ export const Rating = ({
 
     useEffect(() => {
         constructRating(rating);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [rating]);
 
     const constructRating = (currentRating: number) => {
