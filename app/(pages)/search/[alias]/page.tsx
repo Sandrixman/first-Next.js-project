cc987c75a1aa8e2fc6079a03c6c4e947630e6855
Oleksander Novak <sandrix28101981@gmail.com>
2024-01-24 12:50:04 +0200

    deployment microfix

--- app/(pages)/search/[alias]/page.tsx
+++ app/(pages)/search/[alias]/page.tsx
@@ -1,13 +1,20 @@
+// /pages/search/[alias].js
 "use client";
 import { Htag } from "@/components";
 
-export default function Search(): JSX.Element {
+export default function Search({ params }: { params: { alias: string } }): JSX.Element {
+    const { alias } = params;
+
     return (
         <>
-            <Htag tag="h1">Результати пошуку:</Htag>
+            <Htag tag="h1">Результати пошуку для {alias}:</Htag>
             <ul>
                 <li>result</li>
             </ul>
         </>
     );
+}
+
+export async function generateStaticParams() {
+    return [{ params: { alias: "value1" } }, { params: { alias: "value2" } }];
 }
